8b28d0479fa5bb674f7b12897fec60e0d6b37699
Ivan-FD <107582157+Ivan-FD@users.noreply.github.com>
2023-06-26 20:38:34 -0400

    avanceTienda

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tienda_angular';
+
+  http = inject(HttpClient);
+
+  products: Product[] = [];
+
+  changeTitle(){
+    return this.title = "changed";
+  }
+
+  ngOnInit() {
+    this.http.get<Product[]>("https://api.escuelajs.co/api/v1/products")
+    .subscribe((data) => {
+      this.products = data;
+    });
+  }
+
 }
